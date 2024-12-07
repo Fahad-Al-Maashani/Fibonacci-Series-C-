d23ccc2df54a85fc1757b6dc736fd1c77c3bc5e6
Fahad-Al-Maashani <122757915+Fahad-Al-Maashani@users.noreply.github.com>
2024-12-07 08:35:56 +0400

    First initial commit of fibonacci series

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        int n = 10;  // The number of terms you want to display
+        int firstTerm = 0, secondTerm = 1, nextTerm;
+
+        Console.WriteLine("Fibonacci Series:");
+
+        // Loop to generate Fibonacci numbers
+        for (int i = 1; i <= n; ++i)
+        {
+            if (i == 1)
+            {
+                Console.Write(firstTerm + " ");
+                continue;
+            }
+            if (i == 2)
+            {
+                Console.Write(secondTerm + " ");
+                continue;
+            }
+            nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+
+            Console.Write(nextTerm + " ");
+        }
+    }
+}
